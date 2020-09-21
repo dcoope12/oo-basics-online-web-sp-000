858b0b5326fc2996d516a82131d8792066492f26
Devin Cooper <dcoope12@asu.edu>
2020-09-21 22:06:23 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
+class Book
+  attr_accessor :author,:page_count, :genres
+  attr_reader :title
 
+  def iitialize(title)
+    @title = title
+  end
+
+  def turn_page
+    puts "Flipping th page.. wow, you read fast!"
+  end
+end
 
